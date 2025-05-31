4c44c6551a19eeb9dcb0c637aa28c8a39e966934
Sungsik Nam <jmyl@me.com>
2025-05-31 10:05:51 +0200

    Add mini.operators to do substitute

--- init.lua
+++ init.lua
@@ -720,6 +720,7 @@ require('lazy').setup({
 
       -- ... and there is more!
       --  Check out: https://github.com/echasnovski/mini.nvim
+      require('mini.operators').setup()
     end,
   },
   { -- Highlight, edit, and navigate code
