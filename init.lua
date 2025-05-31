14649d52ae91614f2000e074106eff0055deca71
Sungsik Nam <jmyl@me.com>
2025-05-31 21:58:46 +0200

    Revert "Customize telescope keymaps"
    This reverts commit f3a0f84f6b66456bc2e1a64ed519ae613ff0c32f.

--- init.lua
+++ init.lua
@@ -120,7 +120,7 @@ require('lazy').setup({
 
       -- Document existing key chains
       spec = {
-        { '<leader>s', group = '[P]ick' },
+        { '<leader>s', group = '[S]earch' },
         { '<leader>t', group = '[T]oggle' },
         { '<leader>g', group = '[G]it', mode = { 'n', 'v' } },
       },
@@ -202,22 +202,15 @@ require('lazy').setup({
 
       -- See `:help telescope.builtin`
       local builtin = require 'telescope.builtin'
-      vim.keymap.set('n', '<leader>ph', builtin.help_tags, { desc = '[P]ick [H]elp' })
-      vim.keymap.set('n', '<leader>pk', builtin.keymaps, { desc = '[P]ick [K]eymaps' })
-      vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = '[P]ick [F]iles' })
-      --  See `:help telescope.builtin.live_grep()` for information about particular keys
-      vim.keymap.set('n', '<leader>ps', function()
-        builtin.live_grep {
-          grep_open_files = true,
-          prompt_title = '[Pick] in Open Files',
-        }
-      end, { desc = '[P]ick [S]earch results in open files' })
-      vim.keymap.set('n', '<leader>pa', builtin.live_grep { prompt_title = 'Grep' }, { desc = '[P]ick in [A]ll files' })
-      vim.keymap.set('n', '<leader>po', builtin.builtin, { desc = '[P]ick [O]ptions' })
-      vim.keymap.set('n', '<leader>pw', builtin.grep_string, { desc = '[P]ick current [W]ord' })
-      vim.keymap.set('n', '<leader>pd', builtin.diagnostics, { desc = '[P]ick [D]iagnostics' })
-      vim.keymap.set('n', '<leader>pr', builtin.resume, { desc = '[P]ick [R]esume' })
-      vim.keymap.set('n', '<leader>pl', builtin.oldfiles, { desc = '[P]ick from Lo[G]s' })
+      vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+      vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
+      vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+      vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
+      vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
+      vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+      vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+      vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
+      vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
       -- Slightly advanced example of overriding default behavior and theme
@@ -229,10 +222,19 @@ require('lazy').setup({
         })
       end, { desc = '[/] Fuzzily search in current buffer' })
 
+      -- It's also possible to pass additional configuration options.
+      --  See `:help telescope.builtin.live_grep()` for information about particular keys
+      vim.keymap.set('n', '<leader>s/', function()
+        builtin.live_grep {
+          grep_open_files = true,
+          prompt_title = 'Live Grep in Open Files',
+        }
+      end, { desc = '[S]earch [/] in Open Files' })
+
       -- Shortcut for searching your Neovim configuration files
-      vim.keymap.set('n', '<leader>pv', function()
+      vim.keymap.set('n', '<leader>sn', function()
         builtin.find_files { cwd = vim.fn.stdpath 'config' }
-      end, { desc = '[S]earch neo[V]im files' })
+      end, { desc = '[S]earch [N]eovim files' })
     end,
   },
 
