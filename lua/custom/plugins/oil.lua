0290caa737ad3df1072cdddb8384d59b13b23d60
Sungsik Nam <jmyl@me.com>
2025-06-01 17:46:39 +0200

    Make + play <enter>'s role

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -5,6 +5,9 @@ return {
     ---@type oil.SetupOpts
     opts = {
       vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' }),
+      keymaps = {
+        ['+'] = 'actions.select',
+      },
     },
     -- Optional dependencies
     -- dependencies = { { 'echasnovski/mini.icons', opts = {} } },
