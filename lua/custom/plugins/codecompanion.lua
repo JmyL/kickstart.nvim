23004094636707e981bef19e8700d1a58e26bfd2
Sungsik Nam <jmyl@me.com>
2025-06-10 09:13:53 +0200

    Customize codecompanion

--- lua/custom/plugins/codecompanion.lua
+++ lua/custom/plugins/codecompanion.lua
@@ -2,14 +2,125 @@ return {
   {
     'olimorris/codecompanion.nvim',
     cmd = { 'CodeCompanionChat' },
-    keys = {
-      { '<leader>ao', ':CodeCompanionChat Toggle<CR>', desc = '[a]i - [o]pen chat' },
-      { '<leader>aO', ':CodeCompanionChat<CR>', desc = '[a]i - [O]pen new chat' },
-      { '<leader>ah', ':CodeCompanionHistory<CR>', desc = '[a]i - [h]istory' },
-    },
+    lazy = false,
     opts = {
+      display = {
+        window = {
+          layout = 'vertical',
+        },
+        action_palette = {
+          width = 95,
+          height = 10,
+          prompt = 'Prompt ', -- Prompt used for interactive LLM calls
+          provider = 'telescope', -- Can be "default", "telescope", "fzf_lua", "mini_pick" or "snacks". If not specified, the plugin will autodetect installed providers.
+          opts = {
+            show_default_actions = true, -- Show the default actions in the action palette?
+            show_default_prompt_library = true, -- Show the default prompt library in the action palette?
+          },
+        },
+        chat = {
+          auto_scroll = false,
+        },
+      },
       keymaps = {
         show_shortcuts = 'g?',
+      },
+      strategies = {
+        chat = {
+          slash_commands = {
+            ['buffer'] = {
+              keymaps = {
+                modes = {
+                  i = '<C-b>',
+                  n = { '<C-b>' },
+                },
+              },
+            },
+            ['fetch'] = {
+              keymaps = {
+                modes = {
+                  i = '<C-g>',
+                  n = { '<C-g>' },
+                },
+              },
+            },
+            ['file'] = {
+              keymaps = {
+                modes = {
+                  i = '<C-f>',
+                  n = { '<C-f>' },
+                },
+              },
+            },
+            ['image'] = {
+              keymaps = {
+                modes = {
+                  i = '<C-i>',
+                  n = { '<C-i>' },
+                },
+              },
+            },
+            ['now'] = {
+              keymaps = {
+                modes = {
+                  i = '<C-n>',
+                  n = { '<C-n>' },
+                },
+              },
+            },
+            ['help'] = {
+              keymaps = {
+                modes = {
+                  i = '<C-h>',
+                  n = { '<C-h>' },
+                },
+              },
+            },
+            ['symbols'] = {
+              keymaps = {
+                modes = {
+                  i = '<C-o>',
+                  n = { '<C-o>' },
+                },
+              },
+            },
+            ['terminal'] = {
+              keymaps = {
+                modes = {
+                  i = '<C-t>',
+                  n = { '<C-t>' },
+                },
+              },
+            },
+            ['workspace'] = {
+              keymaps = {
+                modes = {
+                  i = '<C-p>',
+                  n = { '<C-p>' },
+                },
+              },
+            },
+          },
+          keymaps = {
+            options = {
+              modes = {
+                n = 'g?',
+              },
+            },
+            send = {
+              modes = { n = '<C-s>', i = '<C-s>' },
+              opts = {},
+            },
+            close = {
+              modes = { n = '<C-c>', i = '<C-c>' },
+              opts = {},
+            },
+            completion = {
+              modes = { i = '<C-l>' },
+              opts = {},
+            },
+          },
+        },
       },
       extensions = {
         history = {
@@ -34,15 +145,16 @@ return {
             enable_logging = false,
           },
         },
-        -- mcphub = {
-        --   callback = 'mcphub.extensions.codecompanion',
-        --   opts = {
-        --     make_vars = true,
-        --     make_slash_commands = true,
-        --     show_result_in_chat = true,
-        --   },
-        -- },
+        mcphub = {
+          callback = 'mcphub.extensions.codecompanion',
+          opts = {
+            make_vars = true,
+            make_slash_commands = true,
+            show_result_in_chat = true,
+          },
+        },
       },
+      language = 'Korean',
     },
     dependencies = {
       'nvim-lua/plenary.nvim',
@@ -51,9 +163,44 @@ return {
       'MeanderingProgrammer/render-markdown.nvim',
       'j-hui/fidget.nvim',
       'ravitemer/codecompanion-history.nvim',
+      {
+        'ravitemer/mcphub.nvim',
+        build = 'npm install -g mcp-hub@latest',
+        config = function()
+          require('mcphub').setup()
+        end,
+      },
     },
     init = function()
       require('config.fidget-spinner'):init()
+      vim.cmd [[cab c CodeCompanion]]
+      vim.cmd [[cab cc CodeCompanionChat]]
+      vim.keymap.set('n', '<leader>ao', ':CodeCompanionChat Toggle<CR>', { desc = '[a]i - t[o]ggle chat' })
+      vim.keymap.set('n', '<leader>aO', ':CodeCompanionChat<CR>', { desc = '[a]i - [O]pen new chat' })
+      vim.keymap.set('n', '<leader>ah', ':CodeCompanionHistory<CR>', { desc = '[a]i - [h]istory' })
+      vim.keymap.set('v', '<leader>ad', ':CodeCompanionChat Add<CR>', { desc = '[a]i - ad[d] to chat' })
+      vim.keymap.set({ 'n', 'v' }, '<leader>al', ':CodeCompanionAction<CR>', { desc = '[a]i - action [l]ist' })
+      vim.keymap.set({ 'n', 'v' }, '<leader>aL', function()
+        vim.api.nvim_feedkeys('V', 'n', false)
+        vim.schedule(function()
+          vim.cmd 'CodeCompanionAction'
+        end)
+      end, { desc = '[a]i - action [L]ist for a file' })
+      vim.keymap.set('n', '<leader>ad', function()
+        vim.api.nvim_feedkeys('V', 'n', false)
+        vim.schedule(function()
+          vim.cmd 'CodeCompanionChat Add'
+        end)
+      end, { desc = '[a]i - ad[d] to chat' })
+
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = 'codecompanion',
+        callback = function()
+          vim.keymap.set('n', '[a', '?^## CodeCompanion<CR>', { buffer = true })
+          vim.keymap.set('n', ']a', '/^## CodeCompanion<CR>', { buffer = true })
+        end,
+      })
+      -- vim.g.codecompanion_auto_tool_mode = true
     end,
   },
 }
