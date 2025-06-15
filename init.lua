45506bc356f98eae197038e9254137bcfba54ad4
Sungsik Nam <jmyl@me.com>
2025-06-15 21:39:45 +0200

    Avoid using <leader>t on lsp

--- init.lua
+++ init.lua
@@ -433,9 +433,9 @@ require('lazy').setup({
           --
           -- This may be unwanted, since they displace some of your code
           if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf) then
-            map('<leader>th', function()
+            map('gh', function()
               vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
-            end, '[T]oggle Inlay [H]ints')
+            end, 'toggle inlay [h]ints')
           end
         end,
       })
