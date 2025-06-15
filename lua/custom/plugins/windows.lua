c52da93ff10128b2897cd08d2063c2ec3424a483
Sungsik Nam <jmyl@me.com>
2025-06-15 13:39:23 +0200

    Add windows

---
+++ lua/custom/plugins/windows.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    'anuvyklack/windows.nvim',
+    lazy = false,
+    keys = {
+      { '<leader>wm', '<cmd>WindowsMaximize<cr>', desc = '[w]indows [m]aximize' },
+    },
+    dependencies = {
+      'anuvyklack/middleclass',
+      'anuvyklack/animation.nvim',
+    },
+    config = function()
+      vim.o.winwidth = 5
+      vim.o.winminwidth = 5
+      vim.o.equalalways = false
+      require('windows').setup {
+        animation = {
+          enable = true,
+          duration = 150,
+          fps = 30,
+          easing = 'in_out_sine',
+        },
+      }
+    end,
+  },
+}
