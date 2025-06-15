c0132b0c21f3c0abace905af0eeee7f8e3fd9119
Sungsik Nam <jmyl@me.com>
2025-06-15 21:40:12 +0200

    Add toggleterm

---
+++ lua/custom/plugins/toggleterm.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    'akinsho/toggleterm.nvim',
+    version = '*',
+    opts = {
+      open_mapping = [[<M-;>]],
+      insert_mappings = false,
+      start_in_insert = false,
+      shade_terminals = false,
+      shading_factor = -1,
+      shading_ratio = -3,
+    },
+    init = function()
+      vim.keymap.set('n', '<leader>ta', ':ToggleTermToggleAll<CR>', { desc = '[t]erminal - toggle [a]ll' })
+      vim.keymap.set('n', '<leader>ts', ':TermSelect<CR>', { desc = '[t]erminal - [s]elect' })
+      -- vim.keymap.set('n', '<leader>tn', function()
+      --   if vim.bo.filetype == 'toggleterm' then
+      --     vim.ui.input({ prompt = 'Set terminal name: ' }, function(input)
+      --       if name ~= '' then
+      --         local term = select(2, require('toggleterm.terminal').identify())
+      --         require('toggleterm').set_term_name(name, term)
+      --       end
+      --     end)
+      --   end
+      -- end, { desc = '[t]erminal - set [n]ame' })
+    end,
+  },
+}
