1f016e7bff033886919c507766b941f0dc3aa8bc
Sungsik Nam <jmyl@me.com>
2025-06-15 21:38:47 +0200

    Customize mini.status_line for toggleterm

--- lua/custom/plugins/mini.lua
+++ lua/custom/plugins/mini.lua
@@ -18,8 +18,7 @@ return {
       require('mini.surround').setup()
 
       -- Simple and easy statusline.
-      --  You could remove this setup call if you don't like it,
-      --  and try some other statusline plugin
+      --  See https://github.com/echasnovski/mini.statusline/blob/main/doc/mini-statusline.txt
       local statusline = require 'mini.statusline'
       -- set use_icons to true if you have a Nerd Font
       statusline.setup { use_icons = vim.g.have_nerd_font }
@@ -32,6 +31,28 @@ return {
         return '%2l:%-2v'
       end
 
+      local orig_section_filename = MiniStatusline.section_filename
+      MiniStatusline.section_filename = function(args)
+        if vim.bo.filetype == 'toggleterm' then
+          local base = 'terminal ' .. tostring(vim.b.toggle_number)
+          local display_name = select(2, require('toggleterm.terminal').identify()).display_name
+          if display_name ~= nil then
+            return base .. ': ' .. display_name
+          else
+            return base
+          end
+        end
+        return orig_section_filename(args)
+      end
+
+      local orig_inactive = MiniStatusline.inactive
+      MiniStatusline.inactive = function(args)
+        if vim.bo.filetype == 'toggleterm' then
+          return MiniStatusline.section_filename(args)
+        else
+          return orig_inactive(args)
+        end
+      end
       -- ... and there is more!
       --  Check out: https://github.com/echasnovski/mini.nvim
       require('mini.operators').setup { replace = { prefix = '<leader>r' } }
