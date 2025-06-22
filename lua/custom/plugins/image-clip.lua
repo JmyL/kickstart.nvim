b37d1c65e61414d39c8c78087cd9d227f134fbfc
Sungsik Nam <jmyl@me.com>
2025-06-22 18:31:40 +0200

    Add image paste plugin

--- lua/custom/plugins/image-clip.lua
+++ lua/custom/plugins/image-clip.lua
@@ -1,6 +1,7 @@
 return {
   {
     'HakonHarnes/img-clip.nvim',
+    ft = { 'markdown', 'codecompanion', 'quarto' },
     opts = {
       filetypes = {
         codecompanion = {
@@ -8,7 +9,22 @@ return {
           template = '[Image]($FILE_PATH)',
           use_absolute_path = true,
         },
+        markdown = {
+          dir_path = 'assets/images',
+          use_absolute_path = false,
+          relative_to_current_file = false,
+          template = '![]($FILE_PATH)',
+        },
+        quarto = {
+          dir_path = 'assets/images',
+          use_absolute_path = false,
+          relative_to_current_file = false,
+          template = '![]($FILE_PATH)',
+        },
       },
     },
+    init = function()
+      vim.keymap.set({ 'n', 'i' }, '<M-i>', '<cmd>PasteImage<cr>', { desc = 'insert [i]mage from clipboard' })
+    end,
   },
 }
