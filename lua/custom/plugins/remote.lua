1d95160d20bb66c79a5997d48f7b86829b102c9f
Sungsik <sungsik@ultihash.io>
2025-12-17 21:31:11 +0100

    Use remote plugin's bug fix version

--- lua/custom/plugins/remote.lua
+++ lua/custom/plugins/remote.lua
@@ -1,7 +1,7 @@
 return {
   {
-    'amitds1997/remote-nvim.nvim',
-    version = '*', -- Pin to GitHub releases
+    'johannes-graner/remote-nvim.nvim',
+    branch = 'fix/pty-argument', -- Pin to GitHub releases
     dependencies = {
       'nvim-lua/plenary.nvim', -- For standard functions
       'MunifTanjim/nui.nvim', -- To build the plugin UI
